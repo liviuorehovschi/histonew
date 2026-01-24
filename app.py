faa57b23ce84f1ef479fdbf58d2081f7b66370c9
liviuorehovschi <liviu.orehovschi@uni.minerva.edu>
2026-01-24 15:23:11 -0800

    Force rebuild v3

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ CLASS_LABELS = {
     'lung_scc': 'Lung Squamous Cell Carcinoma'
 }
 
-print("[INFO] Loading model at startup v2...")
+print("[INFO] Loading model at startup v3...")
 model = tf.keras.models.load_model(MODEL_PATH, compile=False, safe_mode=False)
 print("[SUCCESS] Model loaded once and ready!")
 
